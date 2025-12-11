efd8fb8b10f82c3b147d61a07e82eb9215445480
YuriP123 <miguel.pasamonte2@gmail.com>
2025-12-10 22:11:49 -0800

    build: added vercel package

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "YuriOS",
